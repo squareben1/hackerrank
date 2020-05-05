4ab1bf72f1bc1c2938f010f084497981989a76cf
Squareben1 <bengittins@live.com>
2020-05-05 13:26:00 +0100

    jumping on clouds

---
+++ jumping_on_the_clouds.py
@@ -0,0 +1,49 @@
+
+# Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are 
+# cumulus. She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2. She must avoid 
+# the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. 
+# It is always possible to win the game.
+
+# For each game, Emma will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided. 
+
+# For example, c = [0,1,0,0,0,1,0] indexed from 0..6. 
+
+# The number on each cloud is its index in the list so she must avoid the clouds at indexes 1 and 5. She could follow the following two 
+# paths: 0 -> 2 -> 4 -> 6 or 0 -> 2 -> 3 -> 4 -> 6. 
+
+# The first path takes 3 jumps while the second takes 4.
+
+# Sample Input 0
+
+# 7
+# 0 0 1 0 0 1 0
+
+# Sample Output 0
+
+# 4
+
+
+def jumpingOnClouds(c):
+    n = len(c)
+    ans = 0
+    i = 0
+    while i < n - 1:
+        if i + 2 >= n or c[i + 2] == 1:   # if not possible to make a jump of size 2
+            i = i + 1
+            ans = ans + 1
+        else:
+            i = i + 2
+            ans = ans + 1
+    return ans
+
+# RUBY
+def jumpingOnClouds(c)
+  n = c.length
+  ans = 0
+  i = 0
+  while i < n - 1
+      (i + 2 >= n or c[i + 2] == 1 )? i=i+1: i=i+2
+      ans = ans + 1
+  end
+  return ans
+end
