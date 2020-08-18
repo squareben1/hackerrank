48a9d97caf7e265c77cfd192259147932c7259c8
Squareben1 <bengittins@live.com>
2020-08-18 21:40:43 +0100

    Adds first and second sub arrs

--- 2D_array_ds_hourglass/lib/hourglass_sum.rb
+++ 2D_array_ds_hourglass/lib/hourglass_sum.rb
@@ -1,3 +1,3 @@
 def hourglass_sum(arr)
-  arr[0][0..2].sum
+  arr[0][0..2].sum + arr[1][1]
 end
--- 2D_array_ds_hourglass/spec/hourglass_sum_spec.rb
+++ 2D_array_ds_hourglass/spec/hourglass_sum_spec.rb
@@ -1,8 +1,12 @@
 require 'hourglass_sum'
 
+# [[1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+
 simple_arr = [[1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 
 arr_1 = [[1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+
+arr_2 = [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 
 describe '#hourglassSum' do 
   it 'returns simplest possible - all zeros bar 1' do
@@ -11,5 +15,9 @@ describe '#hourglassSum' do
 
   it 'returns 1st three of 1st arr' do
     expect(hourglass_sum(arr_1)).to eq 3
+  end
+
+  it 'returns 1st three of 1st arr & arr[1][1]' do
+    expect(hourglass_sum(arr_2)).to eq 4
   end
 end
