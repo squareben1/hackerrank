d6752d09dcaa246a8db8e2b77c59e704b9687f9d
Squareben1 <bengittins@live.com>
2020-08-19 13:56:23 +0100

    Added notes

--- 2D_array_ds_hourglass/lib/hourglass_sum.rb
+++ 2D_array_ds_hourglass/lib/hourglass_sum.rb
@@ -1,5 +1,10 @@
 def hourglass_sum(arr)
-  sum = arr[0][0..3].sum + arr[1][1] + arr[2][0..3].sum
+  # first hourglass - NOTE:
+  # description states that the input will be -9 <= R <= 9. Therefore
+  # smallest possible value will be -9*7 (as there are 7 elements in an hourglass)
+  # So one can just initialize the max value to -63 instead of the first hourglass:
+  max = arr[0][0..2].sum + arr[1][1] + arr[2][0..2].sum
+  # for readability:
   col1 = 0
   col4 = 3
   row1 = 0
@@ -7,13 +12,12 @@ def hourglass_sum(arr)
 
   for col in col1..col4
     for row in row1..row4
-      
       hourglass = arr[col][row..row+2].sum + arr[col+1][row+1] + arr[col+2][row..row+2].sum
-      if hourglass > sum
-        sum = hourglass
+      if hourglass > max
+        max = hourglass
       end
     end
   end 
 
-  sum
+  max
 end
--- 2D_array_ds_hourglass/spec/hourglass_sum_spec.rb
+++ 2D_array_ds_hourglass/spec/hourglass_sum_spec.rb
@@ -44,8 +44,13 @@ describe '#hourglassSum' do
     expect(hourglass_sum(acceptance_arr)).to eq 19
   end
 
-  failing_arr = [[0, -4, -6, 0, -7, -6], [-1, -2, -6, -8, -3, -1], [-8, -4, -2, -8, -8, -6], [-3, -1, -2, -5, -7, -4], [-3, -5, -3, -6, -6, -6], [-3, -6, 0, -8, -6, -7]]
+  negative_arr = [[0, -4, -6, 0, -7, -6], [-1, -2, -6, -8, -3, -1], [-8, -4, -2, -8, -8, -6], [-3, -1, -2, -5, -7, -4], [-3, -5, -3, -6, -6, -6], [-3, -6, 0, -8, -6, -7]]
   it 'Returns correct output from failing sample of -ve ints' do
-    expect(hourglass_sum(failing_arr)).to eq -19
+    expect(hourglass_sum(negative_arr)).to eq -19
+  end
+
+  failing_arr = [[7, 6, 8, 2, 4, 3], [7, 3, 3, 0, 6, 1], [3, 8, 7, 7, 2, 2], [0, 8, 6, 8, 6, 1], [7, 1, 6, 0, 2, 4], [2, 7, 8, 1, 7, 4]]
+  it 'Returns correct output from failing sample of -ve ints' do
+    expect(hourglass_sum(failing_arr)).to eq 44
   end
 end
