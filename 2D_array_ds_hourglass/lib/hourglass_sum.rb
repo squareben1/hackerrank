af990be16302c715f4c45f88390a1ae0475ca889
Squareben1 <bengittins@live.com>
2020-08-19 12:18:32 +0100

    Refactor, better naming, removed unnecessary code

--- 2D_array_ds_hourglass/lib/hourglass_sum.rb
+++ 2D_array_ds_hourglass/lib/hourglass_sum.rb
@@ -1,18 +1,18 @@
 def hourglass_sum(arr)
   sum = 0
-  oneMove = arr[0][0..2].sum + arr[1][1] + arr[2][0..2].sum
+  col1 = 0
+  col4 = 3
   row1 = 0
   row4 = 3
 
-  for x in row1..row4
-    for i in 0..3
-      hourglass = arr[x][i..i+2].sum + arr[x+1][i+1] + arr[x+2][i..i+2].sum
+  for col in col1..col4
+    for row in row1..row4
+      hourglass = arr[col][row..row+2].sum + arr[col+1][row+1] + arr[col+2][row..row+2].sum
       if hourglass > sum
         sum = hourglass
       end
     end
   end 
-  
 
-  sum = oneMove > sum ? oneMove : sum
+  sum
 end
