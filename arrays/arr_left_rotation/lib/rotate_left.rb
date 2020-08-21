c0c7dffcb66bbbc7fef6a184201ac3d452fc55ec
Squareben1 <bengittins@live.com>
2020-08-21 09:32:05 +0100

    Added confirmation test and reset method

--- arrays/arr_left_rotation/lib/rotate_left.rb
+++ arrays/arr_left_rotation/lib/rotate_left.rb
@@ -1,6 +1,7 @@
 
 def rotate_left(arr, rot)
   first = arr.shift
+  p first
   arr.push(first)
 end
 
--- arrays/arr_left_rotation/spec/rotate_left_spec.rb
+++ arrays/arr_left_rotation/spec/rotate_left_spec.rb
@@ -3,6 +3,9 @@ require 'rotate_left'
 arr = [1, 2, 3, 4, 5]
 
 describe '#rotate_left' do 
+  before(:each) do
+    arr = [1, 2, 3, 4, 5]
+  end
   it 'returns array unchanged' do
     expect(rotate_left(arr, 0)).to eq arr
   end
@@ -13,5 +16,9 @@ describe '#rotate_left' do
 
   it 'swaps arr of 3 ints 1 rotation' do
     expect(rotate_left([1, 2, 3], 1)).to eq [2, 3, 1]
+  end
+
+  it 'swaps arr of 5 ints 1 rotation' do
+    expect(rotate_left(arr, 1)).to eq [2, 3, 4, 5, 1]
   end
 end
