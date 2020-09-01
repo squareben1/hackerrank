d09c0c1418d2274b068dad3ac1cf0830f87eb6d2
Squareben1 <bengittins@live.com>
2020-09-01 13:25:24 +0100

    Refactor plus brief README with problem

--- other/membershipId/src/CheckDigit.js
+++ other/membershipId/src/CheckDigit.js
@@ -7,11 +7,5 @@ function createCheckDigit(membershipId) {
     return a + b;
   }, 0);
 
-  // if (sum <= 9) {
-  //   return sum;
-  // } else {
-  //   return createCheckDigit(sum.toString());
-  // }
-
   return sum <= 9 ? sum : createCheckDigit(sum.toString());
 }
